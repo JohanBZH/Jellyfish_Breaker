c1585b9af16f155a071f4be308fcdff6ebc0ad53
jmons <johan.mons@viacesi.fr>
2024-10-24 19:21:27 +0200

    Correction des tableaux

--- niveaux.h
+++ niveaux.h
@@ -1,7 +1,7 @@
 #pragma once
 
-extern int niveau1[3][100];   //table pour stocker les coordonnées et présence d'ennemis sur la case
-extern int niveau2[3][100];
+extern int niveau1[3][72];   //table pour stocker les coordonnées et présence d'ennemis sur la case
+extern int niveau2[3][90];
 
 //crée le tableau qui enregistre la position des briques et pilote leur présence
 void init_game();
