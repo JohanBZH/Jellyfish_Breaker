7fc59bb3b56ab9660008f26ddce7a2cebdb80f33
jmons <johan.mons@viacesi.fr>
2024-11-06 10:08:33 +0100

    Script compilation + lancement

---
+++ lancement.sh
@@ -0,0 +1,2 @@
+gcc main.c game.c variables.c niveaux.c sdl_helper/audio_functions.c sdl_helper/constants.c sdl_helper/function.c sdl_helper/text_functions.c -o main.out -lSDL2main -lSDL2 -lm -lSDL2_mixer -lSDL2_ttf
+./main.out
